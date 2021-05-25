95b165e5333068df70774dff81dfdbb8e24849d5
Dan Tra <dltra@fcps.edu>
2021-05-25 14:58:37 -0400

    Create sample Fragments and implement loadFragment() method

---
+++ app/src/main/java/com/example/bottomnavigationbar/FragmentB.java
@@ -0,0 +1,35 @@
+package com.example.bottomnavigationbar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+public class FragmentB extends Fragment {
+    View view;
+
+    //View inflation/instantiation
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a, container,false);
+    }
+    // Setup requiring access to View starts here
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.view = view;
+
+        //setup here
+        TextView textView = view.findViewById(R.id.b_textview);
+        textView.setText("I'm B");
+        Button button = view.findViewById(R.id.b_button);
+        String count = "My Number is 2";
+        button.setText(count);
+    }
+}
--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bottomnavigationbar;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -19,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigationView = findViewById(R.id.navigation_bar);
         toolbar.setTitle("My Title is Lit");
+        loadFragment(new FragmentA());
+    }
+    //standard fragment replacement
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
