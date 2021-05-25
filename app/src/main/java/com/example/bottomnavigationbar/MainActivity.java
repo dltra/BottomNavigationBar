294b7631e4d73f0eb21e885e1b0420cbbbfa47a5
Dan Tra <dltra@fcps.edu>
2021-05-25 14:46:48 -0400

    Setup layouts and BottomNavigationBar

--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.bottomnavigationbar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigationView = findViewById(R.id.navigation_bar);
+        toolbar.setTitle("My Title is Lit");
     }
 }
